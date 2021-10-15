60f052b189ce3377b83ed75cce55c253d0e7b6ec
adewaleK <bisdam91@gmail.com>
2021-10-15 07:14:03 +0100

    SOLUTION TO BRACKET ALGORITHM

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,15 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    while(str.indexOf("()") !== -1 || 
+    str.indexOf("[]") !== -1 || 
+    str.indexOf("{}") !== -1){
+        str = str.replace("()","").replace("[]","").replace("{}","");
+    }
+
+    let isValid = str.length === 0? "valid":"invalid";
+    return isValid;
+}
 
 module.exports = isValid;
