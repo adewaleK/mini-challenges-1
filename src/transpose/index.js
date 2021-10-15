56c6a2c6e0fa336b28e7fe7321c509cb32f28be7
adewaleK <bisdam91@gmail.com>
2021-10-15 07:21:57 +0100

    SOLUTION TO TRANSPOSE  ALGORITHM

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,20 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let resArr = [];
+
+	for(let i=0;i<array[0].length;i++){
+		let innerArr = [];
+        
+		for(let j=0;j<array.length;j++){
+			innerArr.push(array[j][i]);
+	   } 
+
+	   resArr.push(innerArr)
+	}
+
+	return resArr;
+}
 
 module.exports = transpose;
